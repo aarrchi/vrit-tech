b1e0c1451458b2312b4fba7b9b51744bbf36c893
aarrchi <archielama957@gmail.com>
2023-06-27 21:19:06 +0545

    feat:function concepts

--- Day 14/scripts/index.js
+++ Day 14/scripts/index.js
@@ -1,27 +1,65 @@
 
 
-const str = 'madam';
-const length = str.length;
+//******************************************************************* */
+//Functions >> goal 
+//-> specific set of task > specific solution
+//-> removes code repeatation
 
 
-/* for(let i = 0; i<length; i++){
+//es5/es6 -> specification
 
-    console.log(str[i], i);
+//undefined means variable is declared but value is not assigned
+//null means could not find
 
-} */
 
-
-reverseString = '';
-
-for(let i = length-1; i>=0; i-- ){
-   reverseString = reverseString + str[i];
-   debugger;
+//function declare
+function reverseString(num1, num2){ //parameter value passed from down
+    console.log(num1, num2);
 }
 
-if(reverseString === str){
-    console.log(str + ' is a palindrome');
-}else{
-    console.log(str + ' is not a palindrome');
+
+//function call
+reverseString(10, 20); //pass value here(dynamic data)
+
+
+
+/* function reverse(str) {
+    let length = str.length;
+    let reverseString = '';
+    for (i = length-1; i >= 0; i--){
+        reverseString = reverseString + str[i];
+    }
+
+    console.log(reverseString);
+    (reverseString === str ? console.log(str, 'is a palindrome'): console.log(str, 'is not a palindrome'))
 }
 
-/* console.log(reverseString); */
+reverse('madam');
+reverse('abcba');
+reverse('pasang'); */
+
+
+
+//for palidrome numbers
+
+function reverseValue(value) {
+    let str;
+    if(typeof value === 'number' ){
+        str = value.toString();
+    }else{
+        str = value;
+    }
+ 
+    let length = str.length;
+    let reverseString = '';
+    for (i = length-1; i >= 0; i--){
+        reverseString = reverseString + str[i];
+    }
+
+    console.log(reverseString);
+    (reverseString === str ? console.log(str, 'is a palindrome'): console.log(str, 'is not a palindrome'))
+}
+
+
+reverseValue(1222221);
+
